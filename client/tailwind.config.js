81e7228a47bf418b27a75f856a0c7ee1ffaf680a
Chidebe Willie <debesco4u@gmail.com>
2026-07-19 05:51:46 +0100

    CSS fix

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,32 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#16a34a',
-        'primary-focus': '#15803d',
-      },
-    },
-  },
+  theme: { extend: {} },
   plugins: [require('daisyui')],
   daisyui: {
     themes: [{
-      seoaudit: {
-        'primary': '#16a34a',
-        'primary-focus': '#15803d',
-        'primary-content': '#ffffff',
-        'secondary': '#059669',
-        'accent': '#10b981',
-        'neutral': '#191D24',
-        'base-100': '#ffffff',
-        'base-200': '#f3f4f6',
-        'base-300': '#d1d5db',
-        'base-content': '#1f2937',
-        'info': '#3ABFF8',
-        'success': '#16a34a',
-        'warning': '#FBBD23',
-        'error': '#dc2626',
+      seogreen: {
+        "primary": "#16a34a",
+        "primary-content": "#ffffff",
+        "secondary": "#15803d",
+        "accent": "#16a34a",
+        "neutral": "#1f2937",
+        "neutral-content": "#d1d5db",
+        "base-100": "#ffffff",
+        "base-200": "#f9fafb",
+        "base-300": "#e5e7eb",
+        "base-content": "#111827",
+        "info": "#3b82f6",
+        "success": "#16a34a",
+        "warning": "#f59e0b",
+        "error": "#ef4444",
       },
     }],
   },
